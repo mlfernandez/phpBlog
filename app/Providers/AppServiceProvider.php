70d13076159becd3c972147f4060a13fda5e31e6
mlfernandez <mlfernandez@gmail.com>
2021-07-13 10:01:09 +0200

    deploy heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
